55b61447bb1aa5419454e42419e6286de8c593bc
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-05-12 23:16:34 -0300

    Adicionando o DTO e os métodos POST e GET do user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/createUser.dto';
 
 @Controller('user')
 export class UserController {
 
   @Get()
-  async getAllUsers
+  async getAllUsers(){
+    return JSON.stringify({ teste: 'abc'});
+  }
+
+  @Post()
+  async createUser(@Body() createUser: CreateUserDTO){
+    return {
+      ...createUser,
+      password: undefined,
+      }
+  }
+
 }
