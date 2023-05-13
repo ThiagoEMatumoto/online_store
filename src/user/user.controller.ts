1fce267a0257595fa258bcd84f78c25ffd94b087
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-05-13 01:06:38 -0300

    Adicionando criptografia de senha

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,20 +1,21 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { User } from './interfaces/user.interface';
 
 @Controller('user')
 export class UserController {
 
   @Get()
   async getAllUsers(){
-    return JSON.stringify({ teste: 'abc'});
+    return this.userService.getAllUser();
   }
+  
+  constructor(private readonly userService: UserService) {}
 
   @Post()
-  async createUser(@Body() createUser: CreateUserDTO){
-    return {
-      ...createUser,
-      password: undefined,
-      }
+  async createUser(@Body() createUser: CreateUserDTO): Promise<User>{
+    return this.userService.createUser(createUser);
   }
 
 }
---
+++ src/user/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/createUser.dto';
+import { User } from './interfaces/user.interface';
+import { hash } from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  private users: User[] = []
+  async createUser(createUserDTO:CreateUserDTO): Promise<User>{
+    //criptografia da senha
+    const saltOrRounds = 10;
+    const passwordHash = await hash(createUserDTO.password, saltOrRounds);
+    const user ={
+      ...createUserDTO,
+      id: this.users.length + 1,
+      password: passwordHash
+    }
+
+    this.users.push(user)
+
+    return user
+  }
+
+  async getAllUser():Promise<User[]>{
+    return this.users;
+  }
+
+
+}
