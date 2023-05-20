fb10b66fbc6504fc9521c075f658c84a94daf981
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-05-19 23:58:43 -0300

    Algumas Mudanças - não estruturais apenais estéticos no código

--- src/migration/1684204156501-create_table_user.ts
+++ src/migration/1684204156501-create_table_user.ts
@@ -1,9 +1,8 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateTableUser1684204156501 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
             CREATE TABLE public.user (
                 id integer NOT NULL,
                 name character varying NOT NULL,
@@ -28,13 +27,12 @@ export class CreateTableUser1684204156501 implements MigrationInterface {
             ALTER SEQUENCE public.user_id_seq OWNED BY public.user.id;
             
             ALTER TABLE ONLY public.user ALTER COLUMN id SET DEFAULT nextval('public.user_id_seq'::regclass);
-        `)
-    }
+        `);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
             drop table public.user;
-        `)
-    }
-
+        `);
+  }
 }
